acfca4febe011fb28ef2a3c0bb912cf273270949
cesfernandez1 <cesfer9824@gmail.com>
2018-02-27 12:52:09 +0100

    New

---
+++ 2017/05_readline/abetterprompt.cpp
@@ -0,0 +1,18 @@
+#include<readline/readline.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define N 0x100
+
+int main (){
+
+    char buffer[N];
+
+    printf("Nombre: ");
+    fgets(buffer, N, stdin);
+
+    readline("Nombre: ");
+
+	return EXIT_SUCCESS;
+
+}
