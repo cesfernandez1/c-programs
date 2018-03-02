243ee5bcc6c5c161c6f57ab7bbad425b04f34282
cesfernandez1 <cesarfer9824@gmail.com>
2018-03-02 13:27:10 +0100

    LastVersion

--- 2017-2018/03_arrays/fibonachi.cpp
+++ 2017-2018/03_arrays/fibonachi.cpp
@@ -1,7 +1,9 @@
  
 #include <stdlib.h>
 #include <stdio.h>
+
 #define N 20
+
 int main (){
     int a[N];
     a[0] = 1;
@@ -9,7 +11,7 @@ int main (){
     a[3] = (N-1) + (N-2);
     for (int cel=0; cel<N; cel++);
 
-    printf("%i\n", a[3]);
+    printf("%i\n ", a[3]);
 
 	return EXIT_SUCCESS;
 
