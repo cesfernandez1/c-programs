929814aa655f85222c871f6e82396bc28c092296
cesfernandez1 <cesarfer9824@gmail.com>
2018-05-17 14:11:01 +0200

    17/05/2018

---
+++ 2017-2018/22_streams/prueba.cpp
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main (){
+
+    FILE *archivo;
+
+    int nContador = 0;
+    char caracter = 'i';
+
+    archivo = fopen("20729.txt","r");
+    do{
+        if(fgetc(archivo )== caracter)
+            nContador++;
+        }while((caracter = fgetc(archivo)) != EOF);
+
+        printf(" Numero de i = %d\n", nContador);
+        fclose(archivo);
+    
+
+	return EXIT_SUCCESS;
+
+}
